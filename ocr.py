7889362a78c91a2d3fa8677e55bc6d7805db4490
kuuchan-code <kuuchanxn@gmail.com>
2022-06-27 15:31:47 +0900

    modified:   gpu-jupyter.txt new file:   ocr.py new file:   test.py

---
+++ ocr.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+import pytesseract
+
+def ResizeImage(img, magnification):
+    ImgWidth = img.width * magnification
+    ImgHeight = img.height * magnification
+    img_resize = img.resize((int(ImgWidth), int(ImgHeight)), Image.LANCZOS)
+    return img_resize
+
+I = Image.open('test.png')
+I = I.crop((0,0,500,500))
+# I = I.crop((0,50,300,150))
+I = ResizeImage(I, 0.3)
+I.save("test-cropped.png")
+# print(pytesseract.image_to_string(I))
+# print(pytesseract.image_to_string(I, lang="jpn"))
+print(pytesseract.image_to_string(I, lang="jpn", config="-c tessedit_char_whitelist=0123456789m.").split("\n")[0])
