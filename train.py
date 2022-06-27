d19cc99d4537af1942c38744ed35accbe46c7803
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 00:40:32 +0900

    modified:   environment.py modified:   train.py

--- train.py
+++ train.py
@@ -1,20 +1,19 @@
-from stable_baselines.deepq.policies import MlpPolicy
-from stable_baselines import DQN
+import gym
 from environment import AnimalTower
+
+from stable_baselines3 import PPO
 
 env = AnimalTower()
 
-model = DQN(MlpPolicy, env, verbose=1, tensorboard_log="log")
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10_000)
 
-# pathを指定して任意の重みをロードする
-model = DQN.load("./save_weights/rl_model_10000_steps")
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
 
-# 10回試行する
-for i in range(10):
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
-        if dones:
-            break
+env.close()
