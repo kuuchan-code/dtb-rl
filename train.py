d459977dfc098631ebd0c2a62cb062a60065f407
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 01:46:50 +0900

    modified:   train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from stable_baselines3 import A2C
 from environment import AnimalTower
 
-model = A2C("MlpPolicy", AnimalTower()).learn(1)
+model = A2C("MlpPolicy", AnimalTower(), verbose=1, tensorboard_log="./a2c_cartpole_tensorboard/").learn(1)
 model.save("test")
