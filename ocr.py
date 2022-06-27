3b60c103de2b224ba09551e30f6b7aac9fb416a4
kuuchan-code <kuuchanxn@gmail.com>
2022-06-27 18:33:02 +0900

    new file:   main.py modified:   ocr.py

--- ocr.py
+++ ocr.py
@@ -1,18 +1,27 @@
-from PIL import Image
+import cv2
+import numpy as np
 
-import pytesseract
 
-def ResizeImage(img, magnification):
-    ImgWidth = img.width * magnification
-    ImgHeight = img.height * magnification
-    img_resize = img.resize((int(ImgWidth), int(ImgHeight)), Image.LANCZOS)
-    return img_resize
+# 画像の読み込み + グレースケール化
+img = cv2.imread('./tpl_match.png')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-I = Image.open('test.png')
-I = I.crop((0,50,400,450))
-# I = I.crop((0,50,300,150))
-I = ResizeImage(I, 0.3)
-I.save("test-cropped.png")
-# print(pytesseract.image_to_string(I))
-# print(pytesseract.image_to_string(I, lang="jpn"))
-print(pytesseract.image_to_string(I, lang="jpn", config="-c tessedit_char_whitelist=0123456789m.").split("\n"))
+template = cv2.imread('./tpl_match_tpl.png')
+template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+# 処理対象画像に対して、テンプレート画像との類似度を算出する
+res = cv2.matchTemplate(img_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+
+# 類似度の高い部分を検出する
+threshold = 0.8
+loc = np.where(res >= threshold)
+
+# テンプレートマッチング画像の高さ、幅を取得する
+h, w = template_gray.shape
+
+# 検出した部分に赤枠をつける
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+# 画像の保存
+cv2.imwrite('./tpl_match_after.png', img)
