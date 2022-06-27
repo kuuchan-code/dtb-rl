5f3aaf6a7f0da3edf366dc9cd53760883ca2909b
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-27 21:18:08 +0900

    おそらく正しく動作している

--- main.py
+++ main.py
@@ -14,25 +14,26 @@ caps["appium:newCommandTimeout"] = 3600
 caps["appium:connectHardwareKeyboard"] = True
 
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+THRESHOLD = 0.99
 
 try:
     while True:
         driver.save_screenshot('test.png')
-        img_rgb = cv2.imread("test.png")
-        img_rgb = img_rgb[65:129, 0:1080, :]
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        img_bgr = cv2.imread("test.png")
+        img_bgr = img_bgr[65:129, 0:1080, :]
+        img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
         dict_digits = {}
         for i in list(range(10))+["dot"]:
             template = cv2.imread("digits/"+str(i)+".png", 0)
             w, h = template.shape[::-1]
             res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-            threshold = 0.9
-            loc = np.where(res >= threshold)
-            if len(loc[1]) != 0:
-                dict_digits[loc[1].min()] = i
+            loc = np.where(res >= THRESHOLD)
+            print(loc[1])
+            for y in loc[1]:
+                dict_digits[y] = i
             for pt in zip(*loc[::-1]):
                 cv2.rectangle(
-                    img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+                    img_bgr, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
         height = ""
         for key in sorted(dict_digits.items()):
             if key[1] == "dot":
--- template_matching.py
+++ template_matching.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_rgb = cv2.imread("samples/0.66.png")
-img_rgb = img_rgb[65:129, 0:1080, :]
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img_bgr = cv2.imread("samples/1.11_2.png")
+img_bgr = img_bgr[65:129, 0:1080, :]
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
 fig = plt.figure(figsize=(8, 5))
 idx = 1
-threshold = 0.99
+THRESHOLD = 0.99
 min_dist = 20
 
 dict_digits = {}
@@ -19,14 +19,13 @@ for i in list(range(10))+["dot"]:
     template = cv2.imread(f"digits/{i}.png", 0)
     w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    # threshold = 0.99
-    loc = np.where(res >= threshold)
+    loc = np.where(res >= THRESHOLD)
     print(loc[1])
     for y in loc[1]:
         dict_digits[y] = i
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-cv2.imwrite('res.png', img_rgb)
+        cv2.rectangle(img_bgr, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+cv2.imwrite('res.png', img_bgr)
 height = ""
 for key in sorted(dict_digits.items()):
     if key[1] == "dot":
