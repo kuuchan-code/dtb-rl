effeeabc2bd6041684d093eec63f3b41b6b47b12
kuuchan-code <kuuchanxn@gmail.com>
2022-06-27 22:50:05 +0900

    modified:   env.py

--- env.py
+++ env.py
@@ -12,8 +12,8 @@ class CustomEnv(gym.Env):
         test = np.array([v for v in itertools.product(a, b)])
         self.ACTION_MAP = np.array(
             [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]])  # アクションの用意
-        self.action_space = gym.spaces.Discrete(8)       # エージェントが取りうる行動空間を定義
-        self.observationervation_space = ...  # エージェントが受け取りうる観測空間を定義
+        self.action_space = gym.spaces.Discrete(800)       # エージェントが取りうる行動空間を定義
+        self.observation_space = ...  # エージェントが受け取りうる観測空間を定義
         self.reward_range = [-1, 1]       # 報酬の範囲[最小値と最大値]を定義
 
     def reset(self):
@@ -22,7 +22,6 @@ class CustomEnv(gym.Env):
 
     def step(self, action_index):
         # 行動を受け取り行動後の状態をreturnする
-<<<<<<< HEAD
         action = self.ACTION_MAP[action_index]
         # action後の画像(observation)を返す
         # 高くなったら +1
@@ -38,23 +37,7 @@ class CustomEnv(gym.Env):
             reward = -1
             done = True
         return observation, reward, done, {}
-=======
-        if action == 0:
-            pass
-        return obs, reward, done, info
->>>>>>> f48dfa2039efb3f30354bdbded00d9b5e567f8ec
 
-    def render(self, mode='human'):
-        # modeとしてhuman, rgb_array, ansiが選択可能
-        # humanなら描画し, rgb_arrayならそれをreturnし, ansiなら文字列をreturnする
-        ...
-
-    def close(self):
-        ...
-
-    def seed(self, seed=None):
-        ...
-
-
-if __name__ == "__main__":
-    ce = CustomEnv()
+    def render(self):
+        # プレイ画面を返す？
+        pass
