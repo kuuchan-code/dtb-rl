521941698d80ace43aa776a3933c65f5d0d14abb
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 04:37:17 +0900

    modified:   train.py

--- train.py
+++ train.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-from stable_baselines3 import A2C
+from stable_baselines3 import PPO
 from environment import AnimalTower
 import os
 
-for i in range(100):
+for i in range(10):
     print(f"{i+1}回目ループ")
     if os.path.exists("half.zip"):
-        model = A2C.load("half.zip", AnimalTower(),
+        model = PPO.load("half.zip", AnimalTower(),
                          print_system_info=True)
     else:
-        model = A2C("MlpPolicy", AnimalTower(), verbose=1,
-                    tensorboard_log="./a2c_cartpole_tensorboard/")
+        model = PPO("MlpPolicy", AnimalTower(), verbose=1,
+                    tensorboard_log="./ppo_tensorboard/")
     # print(type(model))
-    model.learn(total_timesteps=3)
+    model.learn(total_timesteps=10_0)
     model.save("half")
