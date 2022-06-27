ade02fc6653bf28f985b1736a611a21f891236fb
kuuchan-code <kuuchanxn@gmail.com>
2022-06-27 21:01:41 +0900

    modified:   template_matching.py

--- template_matching.py
+++ template_matching.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 
-img_rgb = cv2.imread("samples/1.38.png")
+img_rgb = cv2.imread("samples/2.52.png")
 img_rgb = img_rgb[65:129, 0:1080, :]
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
@@ -11,10 +11,12 @@ for i in list(range(10))+["dot"]:
     template = cv2.imread("digits/"+str(i)+".png",0)
     w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.9
+    threshold = 0.99
     loc = np.where(res >= threshold)
+    print(loc)
     if len(loc[1]) != 0:
-        dict_digits[loc[1].min()] = i
+        for y in loc[1]:
+            dict_digits[y] = i
     for pt in zip(*loc[::-1]):  
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 cv2.imwrite('res.png',img_rgb)
