f4f771a7276352a96ad4bb6fbbddae20637c0c16
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-27 20:30:53 +0900

    変更

--- template_matching.py
+++ template_matching.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-img_rgb = cv2.imread("samples/1.38.png")
-img_rgb = img_rgb[60:round(280/2), 0:round(600/2), :]
+img_bgr = cv2.imread("samples/2.19.png")
+img_bgr = img_bgr[60:140, 0:300, :]
 dict_digits = {}
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
 for i in list(range(10))+["dot"]:
     template = cv2.imread("digits/"+str(i)+".png", 0)
@@ -16,6 +17,7 @@ for i in list(range(10))+["dot"]:
     if len(loc[1]) != 0:
         dict_digits[loc[1].min()] = i
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-cv2.imwrite('res.png', img_rgb)
+        cv2.rectangle(img_bgr, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imwrite('res.png', img_bgr)
 print(sorted(dict_digits.items()))
