0722edc7ddc291d1d6996d394bc7449c301a08a3
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-27 23:32:52 +0900

    無駄に引数追加

--- trimming.py
+++ trimming.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 import cv2
-import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 
-img_gray = cv2.imread("test.png", 0)
-# img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-# img_bgr = img_bgr[65:129, 0:300]
 
+parser = argparse.ArgumentParser(
+    description="トリミング")    # 2. パーサを作る
+
+# 3. parser.add_argumentで受け取る引数を追加していく
+parser.add_argument("file", help="ファイル名")    # 必須の引数を追加
+# オプション引数（指定しなくても良い引数）を追加
+parser.add_argument("--x0", type=int, default=None)
+parser.add_argument("--x1", type=int, default=None)
+parser.add_argument("--y0", type=int, default=None)
+parser.add_argument("--y1", type=int, default=None)
+
+args = parser.parse_args()    # 4. 引数を解析
+
+print(args.file, args.x0, args.x1, args.y0, args.y1)
+
+# グレースケール読み込み
+img_gray = cv2.imread("test.png", 0)
+
+print(img_gray.shape)
+
+# トリミング
+img_gray = img_gray[1000:, :500]
 
 fig = plt.figure(figsize=(5, 5))
 ax = fig.add_subplot(111)
