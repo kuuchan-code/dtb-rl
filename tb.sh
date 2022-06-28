fa137b54888df7233746dc197b641cedcebd53ae
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 23:04:05 +0900

    modified:   tb.sh

--- tb.sh
+++ tb.sh
@@ -1,1 +1,18 @@
 tensorboard --logdir ./a2c_dtb/
+# ------------------------------------
+# | rollout/              |          |
+# |    ep_len_mean        | 3.81     |
+# |    ep_rew_mean        | 1.25     |
+# | time/                 |          |
+# |    fps                | 0        |
+# |    iterations         | 200      |
+# |    time_elapsed       | 12582    |
+# |    total_timesteps    | 1000     |
+# | train/                |          |
+# |    entropy_loss       | -2.31    |
+# |    explained_variance | 0        |
+# |    learning_rate      | 0.0007   |
+# |    n_updates          | 199      |
+# |    policy_loss        | 0.502    |
+# |    value_loss         | 1.88     |
+# ------------------------------------
