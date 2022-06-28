2b5216cf162d45dacdb013f97e625aeee7752c88
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-28 14:48:19 +0900

    5回で停止

--- train.py
+++ train.py
@@ -4,8 +4,18 @@ from stable_baselines3 import A2C
 from environment import AnimalTower
 import os
 
-model = A2C('MlpPolicy', AnimalTower(), verbose=1,
-            tensorboard_log="./a2c_dtb/")
+# model = A2C('MlpPolicy', AnimalTower(), verbose=1,
+#             tensorboard_log="./a2c_dtb/")
 # model = PPO('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./ppo_dtb/")
+# model.learn(total_timesteps=5)
+# model.save("kuu")
+
+if os.path.exists("test.zip"):
+    model = A2C.load("test.zip", AnimalTower(),
+                     print_system_info=True)
+else:
+    model = A2C("MlpPolicy", AnimalTower(), verbose=1,
+                tensorboard_log="./a2c_cartpole_tensorboard/")
+print(model.get_parameters())
 model.learn(total_timesteps=5)
-model.save("kuu")
+model.save("test")
