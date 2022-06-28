a5c7348dbc473663c9528563a1c8bb97ae0d6e45
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-29 03:12:17 +0900

    ひとまず初期化はできた?

--- sonoda_env_train.py
+++ sonoda_env_train.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-# 面倒なのでtrainも兼ねる
-import os
-from stable_baselines3 import PPO, A2C
 from appium import webdriver
-import itertools
 import gym
 import numpy as np
 from appium import webdriver
@@ -16,18 +12,24 @@ from selenium.webdriver.common.actions import interaction
 from selenium.common.exceptions import InvalidElementStateException, WebDriverException
 
 THRESHOLD = 0.99
-WAITTIME_AFTER_DROP = 8
-WAITTIME_AFTER_RESET = 3
+WAITTIME_AFTER_DROP = 7
+ABOUT_WAITTIME_AFTER_DROP = 3
+WAITTIME_AFTER_RESET = 7
 POLLONG_INTERVAL = 1
 WAITTIME_AFTER_ROTATION = 0.5
-_WAITTIME_AFTER_ROTATION = 0.007
-TAP_TIME = 0.01
+_WAITTIME_AFTER_ROTATION = 0.005
+TAP_TIME = 0.001
 RESET_BUTTON_COORDINATES = 200, 1755
 ROTATE_BUTTON_COORDINATES = 500, 1800
 NUM_OF_DELIMITERS = 30
+TRAIN_WIDTH = 256
+TRAIN_SIZE = int(TRAIN_WIDTH/1920*1080), TRAIN_WIDTH
+NUM_OF_DIV = 32
+SS_NAME = "ss.png"
+OBSERVATION_NAME = "observation.png"
 
 
-def calc_height(img_gray):
+def get_heght(img_gray):
     """
     Height calculation with pattern matching
     """
@@ -46,11 +48,29 @@ def calc_height(img_gray):
             height += "."
         else:
             height += str(key[1])
-    if height:
-        height = float(height)
-    else:
-        height = None
-    return height
+    if not(height):
+        height = 0
+    return float(height)
+
+
+def get_animal_num(img_gray):
+    img_gray_num = img_gray[264:328, :]
+    cv2.imwrite("tesst.png", img_gray_num)
+    dict_digits = {}
+    for i in list(range(10)):
+        template = cv2.imread(f"images/{i}.png", 0)
+        res = cv2.matchTemplate(
+            img_gray_num, template, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= 80)
+        print(loc, i)
+        for y in loc[1]:
+            dict_digits[y] = i
+    animal_num = ""
+    for key in sorted(dict_digits.items()):
+        animal_num += str(key[1])
+    if not(animal_num):
+        animal_num = 0
+    return int(animal_num)
 
 
 def check_record(img_gray):
@@ -68,15 +88,17 @@ def check_record(img_gray):
 
 
 class AnimalTower(gym.Env):
+    act_min = 0
+    act_max = 1
+
     def __init__(self):
         print("Initializing...", end=" ", flush=True)
-        a = np.linspace(0, 7, 8)
-        b = np.linspace(0, 1079, 32)
-        self.ACTION_MAP = np.array([v for v in itertools.product(a, b)])
-        self.action_space = gym.spaces.Discrete(256)
+        # 連続値の座標(?)
+        self.action_space = gym.spaces.Box(
+            self.act_min, self.act_max, shape=(1,), dtype=np.float32)
         self.observation_space = gym.spaces.Box(
-            low=0, high=255, shape=(288, 512))
-        self.reward_range = [-1, 1]
+            low=0, high=255, shape=TRAIN_SIZE[::-1])
+        self.reward_range = [-10, 10]
         self.prev_height = 0
         caps = {}
         caps["platformName"] = "android"
@@ -95,53 +117,51 @@ class AnimalTower(gym.Env):
         print("Resetting...", end=" ", flush=True)
         # Tap the Reset button
         self._tap(RESET_BUTTON_COORDINATES, WAITTIME_AFTER_RESET)
-        img_gray = cv2.imread("test.png", 0)
-        img_gray_resized = cv2.resize(img_gray, dsize=(512, 288))
+        self.driver.save_screenshot(SS_NAME)
+        img_gray = cv2.imread(SS_NAME, 0)
+        img_gray_resized = cv2.resize(img_gray, dsize=TRAIN_SIZE)
+        self.prev_height = get_heght(img_gray)
         observation = img_gray_resized
         # Returns obs after start
         print("Done")
+        cv2.imwrite(OBSERVATION_NAME, observation)
         return observation
 
-    def step(self, action_index):
+    def step(self, action):
         # Perform Action
-        action = self.ACTION_MAP[action_index]
-        print(
-            f"Action being performed({action[0]:.0f}, {action[1]})...", end=" ", flush=True)
-        for _ in range(int(action[0])):
-            self._tap(ROTATE_BUTTON_COORDINATES, _WAITTIME_AFTER_ROTATION)
-        sleep(WAITTIME_AFTER_ROTATION)
-        self._tap((action[1], 800), WAITTIME_AFTER_DROP)
-        print("Done")
-
+        x = np.clip(action, self.act_min, self.act_max)[0] * 1080
+        x.astype(int)
+        print(f"Action({x})")
+        self._tap((x, 800), WAITTIME_AFTER_DROP)
         # Generate obs and reward, done flag, and return
-        self.driver.save_screenshot("test.png")
-        img_gray = cv2.imread("test.png", 0)
-        height = calc_height(img_gray)
-        if check_record(img_gray):
-            observation = cv2.resize(img_gray, dsize=(512, 288))
-            reward = -1
-            done = True
-            print("Game over")
-        else:
-            while height is None:
-                sleep(POLLONG_INTERVAL)
-                self.driver.save_screenshot("test.png")
-                img_gray = cv2.imread("test.png", 0)
-                height = calc_height(img_gray)
-            if height != self.prev_height:
-                observation = cv2.resize(img_gray, dsize=(512, 288))
-                reward = 1
-                done = False
+        for i in range(ABOUT_WAITTIME_AFTER_DROP):
+            self.driver.save_screenshot(SS_NAME)
+            img_gray = cv2.imread(SS_NAME, 0)
+            height = get_heght(img_gray)
+            print(height, self.prev_height)
+            img_gray_resized = cv2.resize(img_gray, dsize=TRAIN_SIZE)
+            observation = img_gray_resized
+            if check_record(img_gray):
+                print("Game over")
+                print("return observation, -1, True, {}")
+                print("-"*NUM_OF_DELIMITERS)
+                cv2.imwrite(OBSERVATION_NAME, observation)
+                return observation, -10, True, {}
+            elif height != self.prev_height:
                 print(f"Height update: {height}m")
+                print("return observation, 1, False, {}")
+                print("-"*NUM_OF_DELIMITERS)
+                self.prev_height = height
+                cv2.imwrite(OBSERVATION_NAME, observation)
+                return observation, height, False, {}
             else:
-                observation = cv2.resize(img_gray, dsize=(512, 288))
-                reward = 0
-                done = False
-                print("No height update")
-            self.prev_height = height
-        print(f"return observation, reward({reward}), done({done}), {{}}")
+                pass
+            sleep(POLLONG_INTERVAL)
+        print("No height update")
+        print("return observation, 0, False, {}")
         print("-"*NUM_OF_DELIMITERS)
-        return observation, reward, done, {}
+        cv2.imwrite(OBSERVATION_NAME, observation)
+        return observation, height, False, {}
 
     def render(self):
         pass
@@ -169,17 +189,4 @@ class AnimalTower(gym.Env):
 
 
 if __name__ == "__main__":
-    for i in range(100):
-        print(f"{i+1}回目ループ")
-        if os.path.exists("half.zip"):
-            model = A2C.load("half.zip", AnimalTower(),
-                             print_system_info=True)
-        else:
-            model = A2C("MlpPolicy", AnimalTower(), verbose=1,
-                        tensorboard_log="./a2c_cartpole_tensorboard/")
-        print(model.get_parameters())
-        model.learn(total_timesteps=20)
-        model.save("half")
-    # model = PPO('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./dtb_ppo/")
-    # model.learn(total_timesteps=10_000)
-    # model.save("kuu")
+    at = AnimalTower()
