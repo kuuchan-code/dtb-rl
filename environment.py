ff7e532268bd9412b658610ca0b0ca94e0a962af
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 12:24:06 +0900

    update

--- environment.py
+++ environment.py
@@ -11,16 +11,22 @@ from selenium.webdriver.common.actions.pointer_input import PointerInput
 from selenium.webdriver.common.actions import interaction
 
 THRESHOLD = 0.99
-
+WAITTIME_AFTER_DROP = 10
+WAITTIME_AFTER_RESET = 1
+WAITTIME_AFTER_ROTATION = 0.1
+POLLONG_INTERVAL = 0.1
+RESET_BUTTON_COORDINATES = 200, 1755
+ROTATE_BUTTON_COORDINATES = 500, 1800
+NUM_OF_DELIMITERS = 30
 
 def calc_height(img_gray):
     """
-    パターンマッチングで高さ計算
+    Height calculation with pattern matching
     """
     img_gray_height = img_gray[65:129, :]
     dict_digits = {}
     for i in list(range(10))+["dot"]:
-        template = cv2.imread(f"digits/{i}.png", 0)
+        template = cv2.imread(f"images/{i}.png", 0)
         res = cv2.matchTemplate(
             img_gray_height, template, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= THRESHOLD)
@@ -39,50 +45,30 @@ def calc_height(img_gray):
     return height
 
 
-def check_guruguru(img_gray):
-    """
-    パターンマッチングでぐるぐるを探す
-    """
-    img_gray_guruguru = img_gray[1600:, :]
-    template = cv2.imread("images/guruguru.png", 0)
-    res = cv2.matchTemplate(
-        img_gray_guruguru, template, cv2.TM_CCOEFF_NORMED)
-    # 判定をゆるくする
-    loc = np.where(res >= 0.9)
-    b = len(loc[1]) > 0
-    return b
-
-
 def check_record(img_gray):
     """
-    パターンマッチングで record を探す
+    Confirmation of termination by recognition of record image
     """
-    template = cv2.imread("digits/record.png", 0)
+    template = cv2.imread("images/record.png", 0)
     res = cv2.matchTemplate(
         img_gray, template, cv2.TM_CCOEFF_NORMED)
     loc = np.where(res >= THRESHOLD)
     if len(loc[1]) > 0:
-        print("ぐるぐるしてる")
         return True
     else:
-        print("ぐるぐるしてない")
-        # b = check_guruguru(img_gray)
-        # if b:
-        #     print("と思ったらぐるぐるしてる")
         return False
 
 
 class AnimalTower(gym.Env):
     def __init__(self):
-        print("初期化")
+        print("Initializing...", end=" ", flush=True)
         a = np.linspace(0, 7, 8)
         b = np.linspace(0, 1079, 32)
         self.ACTION_MAP = np.array([v for v in itertools.product(a, b)])
-        self.action_space = gym.spaces.Discrete(256)       # エージェントが取りうる行動空間を定義
-        self.observation_space = gym.spaces.Box(
-            low=0, high=255, shape=(288, 512))  # エージェントが受け取りうる観測空間を定義
-        self.reward_range = [-1, 1]       # 報酬の範囲[最小値と最大値]を定義
-        self.prev_height = -1  # 初期値変更
+        self.action_space = gym.spaces.Discrete(256)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(288, 512))
+        self.reward_range = [-1, 1]
+        self.prev_height = 0
         caps = {}
         caps["platformName"] = "android"
         caps["appium:ensureWebviewsHavePages"] = True
@@ -93,92 +79,67 @@ class AnimalTower(gym.Env):
         self.operations = ActionChains(self.driver)
         self.operations.w3c_actions = ActionBuilder(
             self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+        print("Done")
 
     def reset(self):
-        print("リセット!!")
-        # リスタートボタンをタップ
-        self._tap(200, 1755)
-        sleep(3)
+        print("Resetting...", end=" ", flush=True)
+        # Tap the Reset button
+        self._tap(RESET_BUTTON_COORDINATES, WAITTIME_AFTER_RESET)
         img_gray = cv2.imread("test.png", 0)
         img_gray_resized = cv2.resize(img_gray, dsize=(512, 288))
         observation = img_gray_resized
-        # スタート後の画像を返す
+        # Returns obs after start
+        print("Done")
         return observation
 
     def step(self, action_index):
-        for i in range(5):
-            self.driver.save_screenshot("test.png")
-            img_gray = cv2.imread("test.png", 0)
-            height = calc_height(img_gray)
-            # 終わり
-            if height is None:
-                if check_record(img_gray):
-                    print("done")
-                    return cv2.resize(img_gray, dsize=(512, 288)), -1, True, {}
-            # 高さ更新
-            elif height > self.prev_height:
-                break
-            sleep(1)
-            print(f"待機中{i}")
-        # actionのようにタップする
+        # Perform Action
         action = self.ACTION_MAP[action_index]
-        # 回数分タップ
         for _ in range(int(action[0])):
-            self._tap(500, 1800)
-            # print("回転")
-        sleep(1)
-        self._tap(action[1], 800)
+            self._tap(ROTATE_BUTTON_COORDINATES, WAITTIME_AFTER_ROTATION)
+        self._tap((action[1], 800), WAITTIME_AFTER_DROP)
 
+        # Generate obs and reward, done flag, and return
         self.driver.save_screenshot("test.png")
         img_gray = cv2.imread("test.png", 0)
         height = calc_height(img_gray)
-        observation = cv2.resize(img_gray, dsize=(512, 288))
-        
-        print(height)
+        if check_record(img_gray):
+            observation = cv2.resize(img_gray, dsize=(512, 288))
+            reward = -1
+            done = True
+            print("Game over")
+        else:
+            while not(height):
+                sleep(POLLONG_INTERVAL)
+                self.driver.save_screenshot("test.png")
+                img_gray = cv2.imread("test.png", 0)
+                height = calc_height(img_gray)
+            if height != self.prev_height:
+                observation = cv2.resize(img_gray, dsize=(512, 288))
+                reward = 1
+                done = False
+                print(f"Height update: {height}m")
+            else:
+                observation = cv2.resize(img_gray, dsize=(512, 288))
+                reward = 0
+                done = False
+                print("No height update")
+            self.prev_height = height
+        print(f"return observation, reward({reward}), done({done}), {{}}")
+        print("-"*NUM_OF_DELIMITERS)
+        return observation, reward, done, {}
 
-        # デフォルトは偽
-        done = False
-        # デフォルトは0
-        reward = 0
-
-        # 報酬を計算
-        for i in range(10):
-            self.driver.save_screenshot("test.png")
-            img_gray = cv2.imread("test.png", 0)
-            height = calc_height(img_gray)
-            print(height, self.prev_height)
-            # なんとなく1秒後を観察
-            if i == 0:
-                observation = cv2.resize(img_gray, (512, 288))
-
-            if height is None:
-                # 落ちた
-                if check_record(img_gray):
-                    print("done")
-                    return observation, -1, True, {}
-            # 高さ更新
-            elif height != self.prev_height:
-                if self.prev_height is not None and height > self.prev_height:
-                    reward = 1
-                self.prev_height = height
-                break
-
-            sleep(1)
-            print(f"更新待機中{i}")
-        # 続行
-        return observation, reward, False, {}
 
     def render(self):
-        # プレイ画面を返す？
         pass
 
-    def _tap(self, x, y):
+    def _tap(self, coordinates, waittime):
         """
-        タップ
+        Tap
         """
-        self.operations.w3c_actions.pointer_action.move_to_location(x, y)
+        self.operations.w3c_actions.pointer_action.move_to_location(coordinates[0], coordinates[1])
         self.operations.w3c_actions.pointer_action.pointer_down()
         self.operations.w3c_actions.pointer_action.pause(0.01)
         self.operations.w3c_actions.pointer_action.release()
         self.operations.perform()
-        sleep(0.01)
+        sleep(waittime)
--- train.py
+++ train.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 from stable_baselines3 import PPO
 from environment import AnimalTower
-import os
 
-for i in range(10):
-    print(f"{i+1}回目ループ")
-    if os.path.exists("half.zip"):
-        model = PPO.load("half.zip", AnimalTower(),
-                         print_system_info=True)
-    else:
-        model = PPO("MlpPolicy", AnimalTower(), verbose=1,
-                    tensorboard_log="./ppo_tensorboard/")
-    # print(type(model))
-    model.learn(total_timesteps=100)
-    model.save("half")
+
+model = PPO('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./ppo_tb/").learn(total_timesteps=3)
