096aeb2215725eb86fa6edddddcb6c1773a7a3d3
kuuchan-code <kuuchanxn@gmail.com>
2022-06-28 17:11:53 +0900

    modified:   environment.py modified:   test.png

--- environment.py
+++ environment.py
@@ -13,16 +13,18 @@ from selenium.webdriver.common.actions import interaction
 THRESHOLD = 0.99
 WAITTIME_AFTER_DROP = 7
 ABOUT_WAITTIME_AFTER_DROP = 3
-WAITTIME_AFTER_RESET = 4
+WAITTIME_AFTER_RESET = 7
 POLLONG_INTERVAL = 1
 WAITTIME_AFTER_ROTATION = 0.5
-_WAITTIME_AFTER_ROTATION = 0.007
-TAP_TIME = 0.01
+_WAITTIME_AFTER_ROTATION = 0.005
+TAP_TIME = 0.001
 RESET_BUTTON_COORDINATES = 200, 1755
 ROTATE_BUTTON_COORDINATES = 500, 1800
 NUM_OF_DELIMITERS = 30
-TRAIN_WIDTH = 512
-TRAIN_SIZE = TRAIN_WIDTH, int(TRAIN_WIDTH/1920*1080)
+TRAIN_WIDTH = 256
+TRAIN_SIZE = int(TRAIN_WIDTH/1920*1080), TRAIN_WIDTH
+SS_NAME = "ss.png"
+OBSERVATION_NAME = "observation.png"
 
 def calc_height(img_gray):
     """
@@ -87,11 +89,13 @@ class AnimalTower(gym.Env):
         self.prev_height = 0
         # Tap the Reset button
         self._tap(RESET_BUTTON_COORDINATES, WAITTIME_AFTER_RESET)
-        img_gray = cv2.imread("test.png", 0)
+        self.driver.save_screenshot(SS_NAME)
+        img_gray = cv2.imread(SS_NAME, 0)
         img_gray_resized = cv2.resize(img_gray, dsize=TRAIN_SIZE)
         observation = img_gray_resized
         # Returns obs after start
         print("Done")
+        cv2.imwrite(OBSERVATION_NAME, observation)
         return observation
 
     def step(self, action):
@@ -103,8 +107,8 @@ class AnimalTower(gym.Env):
         self._tap((action[1], 800), WAITTIME_AFTER_DROP)
         # Generate obs and reward, done flag, and return
         for i in range(ABOUT_WAITTIME_AFTER_DROP):
-            self.driver.save_screenshot("test.png")
-            img_gray = cv2.imread("test.png", 0)
+            self.driver.save_screenshot(SS_NAME)
+            img_gray = cv2.imread(SS_NAME, 0)
             height = calc_height(img_gray)
             img_gray_resized = cv2.resize(img_gray, dsize=TRAIN_SIZE)
             observation = img_gray_resized
@@ -112,12 +116,14 @@ class AnimalTower(gym.Env):
                 print("Game over")
                 print("return observation, -1, True, {}")
                 print("-"*NUM_OF_DELIMITERS)
+                cv2.imwrite(OBSERVATION_NAME, observation)
                 return observation, -1, True, {}
             elif height != self.prev_height:
                 print(f"Height update: {height}m")
                 print("return observation, 1, False, {}")
                 print("-"*NUM_OF_DELIMITERS)
                 self.prev_height = height
+                cv2.imwrite(OBSERVATION_NAME, observation)
                 return observation, 1, False, {}
             else:
                 pass
@@ -125,6 +131,7 @@ class AnimalTower(gym.Env):
         print("No height update")
         print("return observation, 0, False, {}")
         print("-"*NUM_OF_DELIMITERS)
+        cv2.imwrite(OBSERVATION_NAME, observation)
         return observation, 0, False, {}
 
 
