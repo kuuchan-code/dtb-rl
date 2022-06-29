db3489b4e77b66fba92a17d9393543ce88eddfa3
kuuchan-code <kuuchanxn@gmail.com>
2022-06-29 17:19:14 +0900

    modified:   train.py

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from stable_baselines3 import A2C
 from environment import AnimalTower
-# import os
-
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 model = A2C.load(path="kuu-rotate", env=AnimalTower(), tensorboard_log="./a2c_dtb/")
 # model = A2C('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./a2c_dtb/")
-model.learn(total_timesteps=1000)
-model.save("kuu-rotate")
+# Save a checkpoint every 100 steps
+checkpoint_callback = CheckpointCallback(save_freq=100, save_path='./logs/',
+                                         name_prefix='kuu-rotate')
+model.learn(total_timesteps=1000, callback=checkpoint_callback)
