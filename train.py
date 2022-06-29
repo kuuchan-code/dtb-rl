25de53989a14836647fbab0aa96747d8ce66631b
kuuchan-code <kuuchanxn@gmail.com>
2022-06-30 02:00:39 +0900

    modified:   train.py

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 # model = A2C.load(path="a2c_rotate_???step", env=AnimalTower(), tensorboard_log="./a2c_dtb/")
 model = PPO(policy='CnnPolicy', env=AnimalTower(), verbose=1, tensorboard_log="./ppo_tf/")
 # Save a checkpoint every 100 steps
-checkpoint_callback = CheckpointCallback(save_freq=5, save_path='./ppo_logs/',
+checkpoint_callback = CheckpointCallback(save_freq=100, save_path='./ppo_logs/',
                                          name_prefix='rotete_move_12')
 model.learn(total_timesteps=10000, callback=checkpoint_callback)
