39dd3d5a5724846d62d77963a0604c7881ee2992
kuuchan-code <kuuchanxn@gmail.com>
2022-06-29 11:47:44 +0900

    modified:   kuu.zip modified:   tb.sh modified:   train.py

--- tb.sh
+++ tb.sh
@@ -17,3 +17,20 @@ tensorboard --logdir ./a2c_dtb/
 # |    policy_loss        | 0.502    |
 # |    value_loss         | 1.88     |
 # ------------------------------------
+# ------------------------------------
+# | rollout/              |          |
+# |    ep_len_mean        | 4.46     |
+# |    ep_rew_mean        | -2.25    |
+# | time/                 |          |
+# |    fps                | 0        |
+# |    iterations         | 200      |
+# |    time_elapsed       | 12230    |
+# |    total_timesteps    | 1000     |
+# | train/                |          |
+# |    entropy_loss       | -1.68    |
+# |    explained_variance | 0        |
+# |    learning_rate      | 0.0007   |
+# |    n_updates          | 399      |
+# |    policy_loss        | -0.152   |
+# |    value_loss         | 9.82     |
+# ------------------------------------
--- train.py
+++ train.py
@@ -2,20 +2,20 @@
 from stable_baselines3 import A2C
 # from stable_baselines3 import PPO
 from environment import AnimalTower, TRAIN_WIDTH
-import os
+# import os
 
-fname_part = "kuu_copy"
+# fname_part = "kuu_copy"
 
-if os.path.exists(fname_part + ".zip"):
-    model = A2C.load(fname_part + ".zip", AnimalTower(),
-                     print_system_info=True)
-else:
-    model = A2C("MlpPolicy", AnimalTower(), verbose=1,
-                tensorboard_log="./a2c_cartpole_tensorboard/")
-# print(model.get_parameters().keys())
-print(model.get_parameters()["policy"])
-model.learn(total_timesteps=20)
-model.save(fname_part)
+# if os.path.exists(fname_part + ".zip"):
+#     model = A2C.load(fname_part + ".zip", AnimalTower(),
+#                      print_system_info=True)
+# else:
+#     model = A2C("MlpPolicy", AnimalTower(), verbose=1,
+#                 tensorboard_log="./a2c_cartpole_tensorboard/")
+# # print(model.get_parameters().keys())
+# print(model.get_parameters()["policy"])
+# model.learn(total_timesteps=20)
+# model.save(fname_part)
 
 # model = A2C('MlpPolicy', AnimalTower(), verbose=1,
 #             tensorboard_log="./a2c_dtb/")
@@ -27,8 +27,8 @@ model.save(fname_part)
 # model.learn(total_timesteps=20)
 # model.save(f"test_{TRAIN_WIDTH}")
 
-# model = A2C.load("kuu")
+model = A2C.load("kuu", AnimalTower(), tensorboard_log="./a2c_dtb/")
 # model = A2C('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./a2c_dtb/")
-# # model = PPO('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./ppo_dtb/")
-# model.learn(total_timesteps=1000)
-# model.save("kuu")
+# model = PPO('MlpPolicy', AnimalTower(), verbose=1, tensorboard_log="./ppo_dtb/")
+model.learn(total_timesteps=1000)
+model.save("kuu")
