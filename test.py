defd185a22f06c6329845c1fa70e10dd99a69ac3
kuuchan-code <kuuchanxn@gmail.com>
2022-06-30 18:40:33 +0900

    modified:   test.py modified:   train.py

--- test.py
+++ test.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-from stable_baselines.deepq.policies import MlpPolicy
-from stable_baselines import DQN
+from stable_baselines3 import PPO
 from environment import AnimalTower
 
 env = AnimalTower()
 
-model = DQN(MlpPolicy, env, verbose=1, tensorboard_log="log")
-
 # pathを指定して任意の重みをロードする
-model = DQN.load("./save_weights/rl_model_10000_steps")
+model = PPO.load(path="ppo_logs/rotete_move_12_3200_steps", env=env)
 
 # 10回試行する
 for i in range(10):
     obs = env.reset()
     while True:
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
+        obs, rewards, done, info = env.step(action)
         env.render()
-        if dones:
-            break
+        if done:
+            obs = env.reset()
+
--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 from stable_baselines3 import PPO
 from environment import AnimalTower
-from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
+from stable_baselines3.common.callbacks import CheckpointCallback
 
-# model = A2C.load(path="a2c_rotate_???step", env=AnimalTower(), tensorboard_log="./a2c_dtb/")
-model = PPO(policy='CnnPolicy', env=AnimalTower(),
-            verbose=1, tensorboard_log="./ppo_tf/")
-eval_callback = EvalCallback(eval_env=AnimalTower(), best_model_save_path="./ppo_logs/",
-                             log_path="./ppo_logs/", eval_freq=2,
-                             deterministic=True, render=False)
+env = AnimalTower()
+model = PPO.load(path="ppo_logs/rotete_move_12_3200_steps", env=env, tensorboard_log="./a2c_dtb/")
+# model = PPO(policy='CnnPolicy', env=env,
+#             verbose=1, tensorboard_log="./ppo_tf/")
 checkpoint_callback = CheckpointCallback(save_freq=5, save_path='./ppo_logs/',
                                          name_prefix='rotete_move_12')
-model.learn(total_timesteps=10, callback=[
-            eval_callback, checkpoint_callback])
+model.learn(total_timesteps=10, callback=[checkpoint_callback])
