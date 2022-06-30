e7b202b50e01131b158298fbc177f00f7e400992
kuuchan-code <kuuchanxn@gmail.com>
2022-06-30 19:35:41 +0900

    modified:   train.py

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ from environment import AnimalTower
 from stable_baselines3.common.callbacks import CheckpointCallback
 
 env = AnimalTower()
-model = PPO.load(path="ppo_logs/rotete_move_12_3200_steps", env=env, tensorboard_log="./a2c_dtb/")
+model = PPO.load(path="ppo_logs/rotete_8_3200_steps", env=env, tensorboard_log="./ppo_dtb/")
 # model = PPO(policy='CnnPolicy', env=env,
 #             verbose=1, tensorboard_log="./ppo_tf/")
-checkpoint_callback = CheckpointCallback(save_freq=5, save_path='./ppo_logs/',
-                                         name_prefix='rotete_move_12')
-model.learn(total_timesteps=10, callback=[checkpoint_callback])
+checkpoint_callback = CheckpointCallback(save_freq=100, save_path='./ppo_logs/',
+                                         name_prefix='rotete_8')
+model.learn(total_timesteps=1000, callback=[checkpoint_callback])
