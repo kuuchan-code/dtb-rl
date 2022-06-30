8f299f458b74c501de67f29c7b59d356b17e8d3e
kuuchan-code <kuuchanxn@gmail.com>
2022-06-30 19:22:34 +0900

    modified:   test.py

--- test.py
+++ test.py
@@ -7,13 +7,10 @@ env = AnimalTower()
 # pathを指定して任意の重みをロードする
 model = PPO.load(path="ppo_logs/rotete_move_12_3200_steps", env=env)
 
-# 10回試行する
-for i in range(10):
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        env.render()
-        if done:
-            obs = env.reset()
-
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
