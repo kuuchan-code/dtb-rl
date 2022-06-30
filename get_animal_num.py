b597748ba1e323d4a400e23fa9a49c168ba10679
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-01 02:49:45 +0900

    パラメータ微調整

--- get_animal_num.py
+++ get_animal_num.py
@@ -2,11 +2,11 @@
 import cv2
 import numpy as np
 
-THRESHOLD = 0.95
+THRESHOLD = 0.97
 # 背景色 (bgr)
 BACKGROUND_COLOR = np.array([251, 208, 49], dtype=np.uint8)
 BACKGROUND_COLOR_LIGHT = BACKGROUND_COLOR + 4
-BACKGROUND_COLOR_DARK = BACKGROUND_COLOR - 4
+BACKGROUND_COLOR_DARK = BACKGROUND_COLOR - 5
 BLACK = np.zeros(3, dtype=np.uint8)
 WHITE = BLACK + 255
 WHITE_DARK = WHITE - 15
@@ -17,15 +17,10 @@ def counter_shadow_extraction(image: np.ndarray) -> np.ndarray:
     数値の影抽出
     もっと簡単にできなかったか...
     """
-    img_mask = cv2.bitwise_not(cv2.inRange(
-        image, BACKGROUND_COLOR_LIGHT, WHITE_DARK))
-    result = cv2.bitwise_and(image, image, mask=img_mask)
-    img_mask = cv2.bitwise_not(cv2.inRange(
+    img_mask = cv2.inRange(
         image, BACKGROUND_COLOR_DARK, WHITE)
-    )
-    result = cv2.bitwise_not(cv2.bitwise_and(result, result, mask=img_mask))
-    img_mask = cv2.bitwise_not(cv2.inRange(result, BLACK, WHITE - 1))
-    return cv2.cvtColor(cv2.bitwise_and(result, result, mask=img_mask), cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("ttest.png", img_mask)
+    return img_mask
 
 
 def get_animal_num(img_bgr: np.ndarray) -> int:
@@ -33,7 +28,8 @@ def get_animal_num(img_bgr: np.ndarray) -> int:
     動物の数を取得
     引数にはカラー画像を与える!!
     """
-    img_shadow = counter_shadow_extraction(img_bgr)
+    img_trim = img_bgr[264:328, :]
+    img_shadow = counter_shadow_extraction(img_trim)
     dict_digits = {}
     for i in list(range(10)):
         template = cv2.imread(f"images/count{i}_shadow.png", 0)
@@ -53,6 +49,6 @@ def get_animal_num(img_bgr: np.ndarray) -> int:
 
 if __name__ == "__main__":
     # 試したい画像を入れてみる
-    img_bgr = cv2.imread("sonoda/num0_cloud.png")
+    img_bgr = cv2.imread("sonoda/num2_cloud.png")
     n = get_animal_num(img_bgr)
     print(n)
--- train.py
+++ train.py
@@ -6,8 +6,10 @@ from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 env = AnimalTower()
 # model = PPO.load(path="ppo_logs/rotete_8_???_steps",
 #                  env=env, tensorboard_log="./ppo_dtb/")
-model = PPO(policy='CnnPolicy', env=env,
-            verbose=1, tensorboard_log="./ppo_tf/")
+model = PPO.load(path="ppo_logs/rotete_move_12_3200_steps",
+                 env=env, tensorboard_log="./ppo_dtb")
+# model = PPO(policy='CnnPolicy', env=env,
+#             verbose=1, tensorboard_log="./ppo_tf/")
 checkpoint_callback = CheckpointCallback(save_freq=100, save_path='./ppo_logs/',
-                                         name_prefix='rotete_8')
+                                         name_prefix='rotete_move_12')
 model.learn(total_timesteps=1000, callback=[checkpoint_callback])
