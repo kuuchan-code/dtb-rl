77a9dd1c89056455a9671496f0fa14ba041871b7
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-01 02:52:02 +0900

    1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた1行で書けた

--- get_animal_num.py
+++ get_animal_num.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 
-THRESHOLD = 0.97
+THRESHOLD = 0.95
 # 背景色 (bgr)
 BACKGROUND_COLOR = np.array([251, 208, 49], dtype=np.uint8)
 BACKGROUND_COLOR_LIGHT = BACKGROUND_COLOR + 4
@@ -12,24 +12,14 @@ WHITE = BLACK + 255
 WHITE_DARK = WHITE - 15
 
 
-def counter_shadow_extraction(image: np.ndarray) -> np.ndarray:
-    """
-    数値の影抽出
-    もっと簡単にできなかったか...
-    """
-    img_mask = cv2.inRange(
-        image, BACKGROUND_COLOR_DARK, WHITE)
-    cv2.imwrite("ttest.png", img_mask)
-    return img_mask
-
-
 def get_animal_num(img_bgr: np.ndarray) -> int:
     """
     動物の数を取得
     引数にはカラー画像を与える!!
     """
-    img_trim = img_bgr[264:328, :]
-    img_shadow = counter_shadow_extraction(img_trim)
+    img_shadow = cv2.inRange(
+        img_bgr[264:328], BACKGROUND_COLOR_DARK, WHITE)
+    cv2.imwrite("ttest.png", img_shadow)
     dict_digits = {}
     for i in list(range(10)):
         template = cv2.imread(f"images/count{i}_shadow.png", 0)
@@ -49,6 +39,6 @@ def get_animal_num(img_bgr: np.ndarray) -> int:
 
 if __name__ == "__main__":
     # 試したい画像を入れてみる
-    img_bgr = cv2.imread("sonoda/num2_cloud.png")
+    img_bgr = cv2.imread("sonoda/num0_cloud.png")
     n = get_animal_num(img_bgr)
     print(n)
