26177e432809f513719d618c0eb4669548768e7d
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-06-30 22:00:56 +0900

    とりあえずマッチはした modified:   template_matching.py

--- template_matching.py
+++ template_matching.py
@@ -5,16 +5,21 @@ import matplotlib.pyplot as plt
 
 THRESHOLD = 0.99
 
-img_bgr = cv2.imread("samples/5.88.png")
-img_bgr = img_bgr[65:129, 0:300, :]
-img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+# img_bgr = cv2.imread("samples/5.88.png")
+img_bgr = cv2.imread("sonoda/num0.png")
+img_bgr = img_bgr[260:330, 0:300, :]
 
 fig = plt.figure(figsize=(8, 5))
-idx = 1
+ax = fig.add_subplot(3, 4, 1)
+ax.imshow(img_bgr[:, :, ::-1])
+
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+idx = 2
 
 dict_digits = {}
-for i in list(range(10))+["dot"]:
-    template = cv2.imread(f"digits/{i}.png", 0)
+for i in list(range(10)):
+    template = cv2.imread(f"images/count{i}.png", 0)
     w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
     ax = fig.add_subplot(3, 4, idx)
@@ -27,12 +32,10 @@ for i in list(range(10))+["dot"]:
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img_bgr, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 cv2.imwrite('res.png', img_bgr)
+
 height = ""
 for key in sorted(dict_digits.items()):
-    if key[1] == "dot":
-        height += "."
-    else:
-        height += str(key[1])
+    height += str(key[1])
 
 print(height)
 plt.show()
